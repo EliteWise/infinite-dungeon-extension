69bab280d56aa6612634a9d2d2eac743df68c4aa
Florent <eliteman.fm@gmail.com>
2024-03-13 22:27:46 +0100

    Plugin Init

---
+++ src/main/java/fr/elite/infinitedungeon/infinitedungeonextension/Infinite_dungeon_extension.java
@@ -0,0 +1,17 @@
+package fr.elite.infinitedungeon.infinitedungeonextension;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Infinite_dungeon_extension extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
